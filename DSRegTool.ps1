a150cb46e299786c324ba5d2db59c4686719c26d
Mohammad Zmaili <42134639+mzmaili@users.noreply.github.com>
2021-02-04 09:00:23 +0200

    Update DSRegTool.ps1

--- DSRegTool.ps1
+++ DSRegTool.ps1
@@ -47,8 +47,8 @@ Function CheckePRT{
 }
 
 Function PSasAdmin{
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 Function CheckPRT{
